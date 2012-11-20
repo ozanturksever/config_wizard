c6ec8d99e44b5c3b590f970d70c358c7444c13ab
fahreddin yapar <fahreddin.yapar@innotim.com>
2012-11-20 17:59:51 +0200

    select languages e2e test fixed

--- static/test/e2e/scenarios/wizardScenario.js
+++ static/test/e2e/scenarios/wizardScenario.js
@@ -7,6 +7,16 @@ describe('Entry', function () {
         expect(browser().location().url()).toBe('/');
     })
 
+    it('can select turkish', function () {
+        element('#select_tr_button').click();
+        expect(binding('"Thank you for using Logsign." | i18n')).toBe('Logsign\'i kullandiginiz icin tesekkur ederiz.');
+    })
+
+    it('can select english', function () {
+        element('#select_en_button').click();
+        expect(binding('"Thank you for using Logsign." | i18n')).toBe('Thank you for using Logsign.');
+    })
+
     it('should go to next step', function () {
         element('button#goToNextStep').click();
         expect(browser().location().url()).toBe('/selectLanguage');
